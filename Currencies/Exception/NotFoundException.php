9a8c8a695dd3a1eaa9bf65e9e33caf0353c4ba69
João Morais <jcsmorais@gmail.com>
2012-08-24 00:48:53 +0100

    Update license in PHP files
    * Update repository link in license text.

--- Currencies/Exception/NotFoundException.php
+++ Currencies/Exception/NotFoundException.php
@@ -1,12 +1,12 @@
 <?php
 
 /*
- * This file is part of the OpenExchangeRates which is a PHP library for the
+ * This file is part of the Open Exchange Rates library a PHP library for the
  * services provided by http://openexchangerates.org a real-time exchange rates
  * API for developers.
  *
  * Copyright (c) 2012 João Morais
- * http://github.com/jcsmorais/openexchangerates
+ * http://github.com/jcsmorais/open-exchange-rates-lib
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
