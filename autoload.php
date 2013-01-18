6547bf093efb98addefa8de0a83fb2468d8a790c
João Morais <jcsmorais@gmail.com>
2013-01-18 12:33:17 +0000

    Add license docblock to autoload

--- autoload.php
+++ autoload.php
@@ -1,5 +1,20 @@
 <?php
 
+/*
+ * This file is part of the Open Exchange Rates library a PHP library for the
+ * services provided by http://openexchangerates.org a real-time exchange rates
+ * API for developers.
+ *
+ * Copyright (c) 2012 João Morais
+ * http://github.com/jcsmorais/open-exchange-rates-lib
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT
+ *   See LICENSE shipped with this library.
+ */
+
 $loader = require __DIR__ . '/vendor/autoload.php';
 
 return $loader;
